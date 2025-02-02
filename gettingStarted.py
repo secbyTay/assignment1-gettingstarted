3bd4ca47b167a35923c7a0bd75354f76945172e0
Taylor M Smith <tmxmi03@gmail.com>
2025-02-01 20:21:34 -0600

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -24,12 +24,25 @@ if __name__ == "__main__":
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+def welcome_assignment_answers(question):
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+    elif question == "Are encoding and encryption the same? - Yes/No": 
+        answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No": 
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No": 
+        answer = "Yes"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No": 
+        answer = "No"
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ": 
+        answer = "ddfcf4d77f5eb094dc8ee4582098ba6c8edd92b06e48bb455e878e880a613f48"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No": 
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number": 
+        answer = 7
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number": 
+        answer = 3
+    else:
+        "The answers are incorrect"
+    return(answer)
